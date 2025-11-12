4ad975e703b6870d594507b965b4007e2c6a7883
nsh15-cmd <183855811+nsh15-cmd@users.noreply.github.com>
2025-11-12 10:47:51 +0800

    commited wag na mag error

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,19 +1,17 @@
-import {Link} from "react-router";
+import { Link } from "react-router";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar ">
-            <Link to="/">
-<<<<<<< HEAD
-                <p className="text-2xl font-bold text-gradient ">IAIN - Resume Assitant</p>
-=======
-                <p className="text-2xl font-bold text-gradient">IAIN - Resume Analyzer</p>
->>>>>>> 433c5d9be65a251e8adf712c37b65bf297570fb9
-            </Link>
-            <Link to="/upload" className="primary-button w-fit">
-                Upload Resume
-            </Link>
-        </nav>
-    )
-}
-export default Navbar
+  return (
+    <nav className="navbar ">
+      <Link to="/">
+        <p className="text-2xl font-bold text-gradient ">
+          IAIN - Resume Analyzer
+        </p>
+      </Link>
+      <Link to="/upload" className="primary-button w-fit">
+        Upload Resume
+      </Link>
+    </nav>
+  );
+};
+export default Navbar;
